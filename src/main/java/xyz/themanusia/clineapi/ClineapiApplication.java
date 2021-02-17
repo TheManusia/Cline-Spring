8318bf93fe0fa7b4a8ee66daa07397a05b5fde5c
Ian <minecraftindo0@gmail.com>
2021-02-17 21:08:23 +0700

    Project Initialize

---
+++ src/main/java/xyz/themanusia/clineapi/ClineapiApplication.java
@@ -0,0 +1,13 @@
+package xyz.themanusia.clineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClineapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClineapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/themanusia/clineapi/ClineapiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.themanusia.clineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClineapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
